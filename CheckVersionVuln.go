96612adb68b138f6d3feb2caad0f7c5c483c5c54
Eduardo Barbosa (an4kein) <37910997+an4kein@users.noreply.github.com>
2021-03-05 15:38:11 -0800

    Create CheckVersionVuln.go

---
+++ CheckVersionVuln.go
@@ -0,0 +1,35 @@
+package main
+
+
+// Author @anakein
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func CheckVersionVuln() {
+	sessao1 := "retorno de uma funcao"
+	cookie := 0 != 1337
+
+	compare, _ := strconv.ParseInt(sessao1, 10, 64)
+	newcompare := 0 != compare
+
+	switch auto := true; auto {
+	case auto == true:
+		if !(cookie == false && newcompare == false) {
+			fmt.Println("[+] Versao vulnerable")
+
+		} else {
+			fmt.Println("[-] not vulnerable")
+		}
+	case auto == true:
+		fmt.Println("EH falso")
+	}
+
+	return
+}
+
+func main() {
+	CheckVersionVuln()
+}
